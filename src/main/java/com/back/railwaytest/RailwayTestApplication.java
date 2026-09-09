906e7db6f2d96e76aec26f1cc147bd6fbe8b0d8b
jihoo414-tech <jihoo414@gmail.com>
2026-09-09 22:42:31 +0900

    초기화

---
+++ src/main/java/com/back/railwaytest/RailwayTestApplication.java
@@ -0,0 +1,13 @@
+package com.back.railwaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayTestApplication.class, args);
+    }
+
+}
